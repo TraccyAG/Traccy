ea1f4fbdc8e17bbf5ec91bc69c18d99bf6676411
Andrii Millovich <triplle@ukr.net>
2023-06-09 19:17:48 +0300

    add KYC integration

--- src/containers/Invest/index.js
+++ src/containers/Invest/index.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Steps } from 'antd';
-import { SvgIcon } from '../../components/common';
+import React, {useEffect, useState} from 'react';
+import {Link, useLocation} from 'react-router-dom';
+import {Button, Steps} from 'antd';
+import {SvgIcon} from '../../components/common';
 import InvestStep1 from './InvestStep1';
 import InvestStep2 from './InvestStep2';
 import InvestStep3 from './InvestStep3';
 import InvestStep4 from './InvestStep4';
 import './index.scss';
 
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 const Invest = () => {
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const [current, setCurrent] = useState(0);
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
 
+    // Get the value of a specific query parameter
+    const jwt = searchParams.get('jwt');
+    const localJwt = localStorage.getItem('jwt')
+    useEffect(() => {
+        if (!jwt && !localJwt) {
+            window.location.href = 'https://in.sumsub.com/idensic/l/#/uni_QUqyWtzT5Evcg0eC';
+        } else {
+            localStorage.setItem('jwt', jwt)
+        }
+
+    }, []);
     const next = () => {
         setCurrent(current + 1);
     };
@@ -24,50 +37,50 @@ const Invest = () => {
 
     const steps = [
         {
-            title: t("buy:choose"),
-            content: <InvestStep2 onNext={next} onPrev={prev} />,
-        },
-        {
             title: `Purchase Agreement`,
-            content: <InvestStep1 onNext={next} />,
+            content: <InvestStep1 onNext={next}/>,
         },
         {
-            title: t("buy:fill"),
-            content: <InvestStep3 onNext={next} onPrev={prev} />,
+            title: t("buy:choose"),
+            content: <InvestStep2 onNext={next} onPrev={prev}/>,
         },
-        {
-            title: t("buy:confirmation"),
-            content: <InvestStep4 onPrev={prev} />,
-        }
+        // {
+        //     title: t("buy:fill"),
+        //     content: <InvestStep3 onNext={next} onPrev={prev}/>,
+        // },
+        // {
+        //     title: t("buy:confirmation"),
+        //     content: <InvestStep4 onPrev={prev}/>,
+        // }
     ];
 
-    const items = steps.map((item) => ({ key: item.title, title: item.title }));
+    const items = steps.map((item) => ({key: item.title, title: item.title}));
     return (
         <div className='investform-wrapper'>
             <div className="polygon-effect">
-                <img src="/invest-form/left-hexa.png" alt="polygon-left" />
-                <img src="/invest-form/right-hexa.png" alt="polygon-right" />
+                <img src="/invest-form/left-hexa.png" alt="polygon-left"/>
+                <img src="/invest-form/right-hexa.png" alt="polygon-right"/>
             </div>
             <div className='leftbar'>
                 <div className='social-icon-list'>
                     <a href="https://twitter.com/traccy_official?s=11&t=_z6GdVt91PmJiJmxDvd8sA">
-                        <SvgIcon name='twitter' viewbox='0 0 36 29.239' />
+                        <SvgIcon name='twitter' viewbox='0 0 36 29.239'/>
                     </a>
                     <a href="https://web.telegram.org/z/#-1837824968">
-                        <SvgIcon name='telegram' viewbox='0 0 34.875 34.664' />
+                        <SvgIcon name='telegram' viewbox='0 0 34.875 34.664'/>
                     </a>
                     <a href="https://www.instagram.com/traccy_official/">
-                        <SvgIcon name='instagram' viewbox='0 0 32.999 32.999' />
+                        <SvgIcon name='instagram' viewbox='0 0 32.999 32.999'/>
                     </a>
                     <a href="https://www.linkedin.com/company/traccy-ag/?viewAsMember=true">
-                        <SvgIcon name='linkedin' viewbox='0 0 32.001 32.001' />
+                        <SvgIcon name='linkedin' viewbox='0 0 32.001 32.001'/>
                     </a>
                 </div>
             </div>
             <div className='right-section'>
                 <div className='rightbar-inner'>
                     <div className='dtl-section'>
-                        <Steps current={current} items={items} />
+                        <Steps current={current} items={items}/>
                         <div className="steps-content">{steps[current].content}</div>
                     </div>
                 </div>
