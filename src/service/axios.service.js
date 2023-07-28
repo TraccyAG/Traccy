bc31421a2fecb59c9ff0e1e62c4a602e49a60054
Andrii Millovich <triplle@ukr.net>
2023-07-28 17:45:52 +0300

    1-5 task

--- src/service/axios.service.js
+++ src/service/axios.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//export const baseUrl= 'https://octopus-app-z7hd5.ondigitalocean.app/'
-export const baseUrl= 'http://localhost:5000'
+export const baseUrl= 'https://octopus-app-z7hd5.ondigitalocean.app/'
+// export const baseUrl= 'http://localhost:5000'
 
 export const axiosService = axios.create({
     baseURL: baseUrl,
